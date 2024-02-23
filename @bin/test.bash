fcb0be31e00d4cded217d0180aa1353b51b6577d
Greg Rynkowski <5878299+rynkowsg@users.noreply.github.com>
2024-02-22 23:52:47 -0300

    Add repo-l2 (master-lfs)

--- @bin/test.bash
+++ @bin/test.bash
@@ -20,6 +20,9 @@ SCRIPT_DIR="$(cd "$(dirname "${SCRIPT_PATH}")" || exit 1; pwd -P)"
 REPO_DIR="$(cd "${SCRIPT_DIR}/.." || exit 1; pwd -P)"
 # detect REPO_DIR - end
 
+# LFS_ENABLED - Git LFS support, if not specified, set to false
+LFS_ENABLED=${LFS_ENABLED:-0}
+
 # SUBMODULES_ENABLED - submodules support, if not specified, set to false
 SUBMODULES_ENABLED=${SUBMODULES_ENABLED:-0}
 
@@ -28,4 +31,7 @@ echo "Running: ${REPO_DIR}/test/test_content.bats..."
 
 if [ "${SUBMODULES_ENABLED}" = 1 ]; then
   "${REPO_DIR}/refs/repo-l2/@bin/test.bash"
+fi
+if [ "${SUBMODULES_ENABLED}" = 1 ] && [ "${LFS_ENABLED}" = 1 ]; then
+  "${REPO_DIR}/refs/repo-l2-lfs/@bin/test.bash"
 fi
